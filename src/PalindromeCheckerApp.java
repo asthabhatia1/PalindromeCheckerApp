1b2674026475701ca937cc6fde760242a297a56c
Astha <astha18bhatia@gmail.com>
2026-02-19 10:06:47 +0530

    UC5: using character array and two pointer technique

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,25 +1,35 @@
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
+    public static boolean isPalindrome(String input) {
+
+        char[] character = input.toCharArray();
+
+        int left = 0;
+        int right = character.length - 1;
+
+        while (left < right) {
+            if (character[left] != character[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a word: ");
+
+        System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        int start = 0;
-        int end = input.length() - 1;
-
-        boolean isPalindrome = true;
-        while (start < end) {
-            if (input.charAt(start) != input.charAt(end)) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
+        if (isPalindrome(input)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
         }
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
 
         sc.close();
     }
